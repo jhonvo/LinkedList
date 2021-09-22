abc0b091d95d8f806b677c17b755154c13c0fb1c
Alfredo08 <alfredosv08@gmail.com>
2021-09-22 10:43:08 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+from LinkedList import LinkedList
+
+# At this point the list is empty
+listOfNumbers = LinkedList()
+
+# Adding the first node to the list
+listOfNumbers.insertLast( 10 )
+listOfNumbers.insertLast( 30 )
+listOfNumbers.insertLast( 50 )
+listOfNumbers.insertLast( 80 )
+
+listOfNumbers.printList()
+
